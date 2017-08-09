904d78a16f768c461dc26a50b427bab022e73a9a
Gerald Martinez <gmartinez@versatec.biz>
2017-08-09 17:43:46 -0600

    Model Class

---
+++ NicaWallet/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NicaWallet.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string AccountName { get; set; }
+        public int AccountTypeId { get; set; }
+        public double StartingAmount { get; set; }
+        public int CurrencyId { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? LastUpdate { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
---
+++ NicaWallet/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NicaWallet.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int ParentId { get; set; }
+        public bool? IsParent { get; set; }
+    }
+}
